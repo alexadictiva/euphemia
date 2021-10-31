c27b793b7936a665baba123d0db4a86f7031a66e
Libia Perez <alexandra220790@gmail.com>
2021-10-31 19:59:06 -0300

    adding backend form

--- js/main.js
+++ js/main.js
@@ -18,3 +18,29 @@ $(".faqsContainer").on("click", "button", function () {
   tp.slideToggle();
   p.slideUp();
 });
+
+/*Form*/
+
+const form = document
+  .querySelector("#form")
+  .addEventListener("submit", handleSubmit);
+const mailto = document.querySelector("#mailto");
+
+function handleSubmit(event) {
+  event.preventDefault();
+
+  const formulario = new FormData(this);
+  console.log(formulario.get("name"));
+  console.log(formulario.get("address"));
+  console.log(formulario.get("phone"));
+  console.log(formulario.get("message"));
+
+  mailto.setAttribute(
+    "href",
+    `mailto:academiatrucking@gmail.com?subject=${formulario.get(
+      "name"
+    )}, ${formulario.get("email")}&body=${formulario.get("message")}`
+  );
+
+  mailto.click();
+}
