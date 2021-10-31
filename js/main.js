5823579bf6e75ab4a045fe8cb39e07dcb235a9af
Libia Perez <alexandra220790@gmail.com>
2021-10-31 19:50:25 -0300

    Adding general styles, and generic colors

--- js/main.js
+++ js/main.js
@@ -7,3 +7,14 @@ function showMenu() {
   menu.classList.toggle("hidden");
   overlay.classList.toggle("overlay");
 }
+/* Swiper1 */
+var swiper = new Swiper(".mySwiper", {});
+
+/*FAQs Acordeon JQuery*/
+$(".faqsContainer").on("click", "button", function () {
+  var t = $(this);
+  var tp = t.next();
+  var p = t.parent().siblings().find("p");
+  tp.slideToggle();
+  p.slideUp();
+});
